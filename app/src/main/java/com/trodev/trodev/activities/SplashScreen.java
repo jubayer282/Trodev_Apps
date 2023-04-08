e5204ac0170a7ce673396d80c720447cdc80d2c2
zobayerdev <zobayer.dev@gmail.com>
2023-04-09 03:33:34 +0600

    home, and about us file are complete -nayem

---
+++ app/src/main/java/com/trodev/trodev/activities/SplashScreen.java
@@ -0,0 +1,16 @@
+package com.trodev.trodev.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.trodev.trodev.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
